3f7d5fa5364f38ac89e14b3e85deba3f524ee99a
Paweł Bylica <pawel@hepcolgum.band>
2022-06-17 22:05:04 +0200

    PRECOMPILES cost analysis

--- test/state/precompiles.hpp
+++ test/state/precompiles.hpp
@@ -1,7 +1,6 @@
 // evmone: Fast Ethereum Virtual Machine implementation
 // Copyright 2022 The evmone Authors.
 // SPDX-License-Identifier: Apache-2.0
-
 #pragma once
 
 #include <evmc/evmc.hpp>
@@ -9,5 +8,11 @@
 
 namespace evmone::state
 {
+struct ExecutionResult
+{
+    evmc_status_code status_code;
+    size_t output_size;
+};
+
 std::optional<evmc::result> call_precompile(evmc_revision rev, const evmc_message& msg) noexcept;
-}
+}  // namespace evmone::state
